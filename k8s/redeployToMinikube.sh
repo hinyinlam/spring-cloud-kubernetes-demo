a1e78d75e49e0b235dec3ae438d1b01fa971bf98
Hin Lam <3368889+hinyinlam@users.noreply.github.com>
2022-10-12 18:57:48 +0800

    fix image pull backoff and added startup prob to make sure liveness and readienss start after startup success

--- k8s/redeployToMinikube.sh
+++ k8s/redeployToMinikube.sh
@@ -5,6 +5,8 @@ SCRIPT_PATH=$(dirname "$0")
 eval $(minikube -p minikube docker-env);
 ./mvnw clean spring-boot:build-image;
 
+kubectl apply -f "$SCRIPT_PATH"/rbac-spring-cloud-k8s.yaml 
+
 kubectl delete -f "$SCRIPT_PATH"/k8s-spring-configmap.yaml;
 kubectl create -f "$SCRIPT_PATH"/k8s-spring-configmap.yaml;
 kubectl delete -f "$SCRIPT_PATH"/spring-k8s-demo-deployment.yaml;
